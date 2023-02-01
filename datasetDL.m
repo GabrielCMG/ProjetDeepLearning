f81cd410b6f4f3554cb3234c45906fd041c8da88
Gabriel <gabriel.gueganno@ensta-bretagne.org>
2023-02-01 11:43:07 +0100

    Utilisation de la STFT au lieu de la fonction pretty_spectrogram + calcule de la loss sur les signaux 1D Ne fonctionne pas

--- datasetDL.m
+++ datasetDL.m
@@ -10,21 +10,22 @@ t = 0:T/N:T-T/N;
 
 for i = 1:5
     fList = rand([3, 1])*10;
-    AList = rand([3, 1])+1;
+    AList = [1 1 1];
     s = AList(1)*cos(2*pi*fList(1)*t) + AList(2)*cos(2*pi*fList(2)*t) + AList(3)*cos(2*pi*fList(3)*t); 
-    sB = s + wgn(1, 4096, 10);
+    sB = s + wgn(1, 4096, 0);
     subplot(5, 2, 2*i-1)
     plot(t, s)
     subplot(5, 2, 2*i)
     plot(t, sB)
 end
 
+%%
 
 for i = 1:1000
     fList = rand([3, 1])*10;
-    AList = rand([3, 1])+1;
+    AList = [1 1 1];
     s = AList(1)*cos(2*pi*fList(1)*t) + AList(2)*cos(2*pi*fList(2)*t) + AList(3)*cos(2*pi*fList(3)*t); 
-    sB = s + wgn(1, 4096, 5);
+    sB = s + wgn(1, 4096, 0);
     sigList(i, :) = s;
     sigListNoise(i, :) = sB;
 end
