1df66f758a2317a50c19f6bbae6fa365168bf451
Gabriel <gabriel.gueganno@ensta-bretagne.org>
2023-02-07 14:01:03 +0100

    AE qui marche à peu près + dataset signaux une fréquence

--- datasetDL.m
+++ datasetDL.m
@@ -1,6 +1,6 @@
 %%
 
-Fe = 40;
+Fe = 80;
 N = 4096;
 T = N/Fe;
 
@@ -9,9 +9,9 @@ t = 0:T/N:T-T/N;
 
 
 for i = 1:5
-    fList = rand([3, 1])*10;
+    fList = (rand([3, 1])+1)*5;
     AList = [1 1 1];
-    s = AList(1)*cos(2*pi*fList(1)*t) + AList(2)*cos(2*pi*fList(2)*t) + AList(3)*cos(2*pi*fList(3)*t); 
+    s = AList(1)*cos(2*pi*fList(1)*t);% + AList(2)*cos(2*pi*fList(2)*t) + AList(3)*cos(2*pi*fList(3)*t); 
     sB = s + wgn(1, 4096, 0);
     subplot(5, 2, 2*i-1)
     plot(t, s)
@@ -22,13 +22,13 @@ end
 %%
 
 for i = 1:1000
-    fList = rand([3, 1])*10;
+    fList = (rand([3, 1])+1)*5;
     AList = [1 1 1];
-    s = AList(1)*cos(2*pi*fList(1)*t) + AList(2)*cos(2*pi*fList(2)*t) + AList(3)*cos(2*pi*fList(3)*t); 
+    s = AList(1)*cos(2*pi*fList(1)*t);% + AList(2)*cos(2*pi*fList(2)*t) + AList(3)*cos(2*pi*fList(3)*t); 
     sB = s + wgn(1, 4096, 0);
     sigList(i, :) = s;
     sigListNoise(i, :) = sB;
 end
 
-csvwrite('label.csv', sigList);
-csvwrite('train.csv', sigListNoise);
+csvwrite('label1f.csv', sigList);
+csvwrite('train1f.csv', sigListNoise);
