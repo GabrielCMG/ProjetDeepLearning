4c1f58895ef58fdf49331fb198c30d534c243e50
HugoFvl <91737033+HugoFvl@users.noreply.github.com>
2023-02-09 13:35:37 +0100

    Ajout commentaire génération des signaux.

--- datasetDL.m
+++ datasetDL.m
@@ -1,24 +1,25 @@
 %%
 
-Fe = 256;
-N = 4096;
-T = N/Fe;
+Fe = 256;   %frequence d'echantillonage
+N = 4096;   %Cardinal du signal
+T = N/Fe;   %Temps d'observation
 
 t = 0:T/N:T-T/N;
 
-nFreq = 3;
+nFreq = 1;  %Nombre de frequence d'un signal (nombre entier strictement positif)
 
+%Generation des signaux avec nFreq frequence
 for i = 1:1000
-    fList = (rand([nFreq, 1])+0.2)*5;
-    A = 1;
+    fList = (rand([nFreq, 1])+0.2)*5;   %tirage aléatoirede la frequence
+    A = 1;  %Amplitude associe a chaque frequence
     s = zeros(1, 4096);
     for j=1:nFreq
-        s = s + A*cos(2*pi*fList(j)*t);
+        s = s + A*cos(2*pi*fList(j)*t); %Generation frequence par frequence
     end
-    sB = awgn(s, -7, 'measured');
+    sB = awgn(s, 5, 'measured');    %Ajout d'un bruit blanc gaussien
     sigList(i, :) = s;
     sigListNoise(i, :) = sB;
 end
 
-csvwrite('label3f.csv', sigList);
-csvwrite('train3f.csv', sigListNoise);
+csvwrite('label3f.csv', sigList);   %Enregistrement du signal non bruite
+csvwrite('train3f.csv', sigListNoise);  %Enregistrement du signal bruite
